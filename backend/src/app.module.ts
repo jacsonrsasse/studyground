733fb2b1eba053d4ce96dc2c9365d2bc44579973
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-03-11 21:17:17 -0300

    feat: removed gateway module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { EnvModule } from './env/env.module';
-import { GatewayModule } from './gateway/gateway.module';
 import { BooksModule } from './books/books.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [EnvModule, GatewayModule, BooksModule],
+  imports: [
+    EnvModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: false,
+    }),
+    BooksModule,
+  ],
   controllers: [],
   providers: [],
 })
