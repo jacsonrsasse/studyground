2b435cb95ce85098c02c98f0d5600d11161e19d7
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-03-14 01:15:31 -0300

    feat: added new env to specify temp folder

--- backend/src/books/books.module.ts
+++ backend/src/books/books.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { SseModule } from 'src/sse/sse.module';
+
 import { BooksResolver } from './books.resolver';
 import { BooksController } from './books.controller';
 import { AllowedSheetTypesPipe } from './pipes/allowed-sheet-types.pipe';
 import { BookUseCases } from './use-cases';
 import { BookEventHandlers } from './events';
-import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [CqrsModule, SseModule],
   providers: [
     BooksResolver,
     AllowedSheetTypesPipe,
--- backend/src/env/schema.ts
+++ backend/src/env/schema.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   PORT: z.coerce.number(),
+  TEMP_FOLDER: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
