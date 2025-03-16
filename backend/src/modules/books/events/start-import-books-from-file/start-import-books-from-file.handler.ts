dfc5d1e6be6a2ca8057a244964611f3dccdeea5d
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-03-16 04:07:32 -0300

    fix: read file in stream

--- backend/src/modules/books/events/start-import-books-from-file/start-import-books-from-file.handler.ts
+++ backend/src/modules/books/events/start-import-books-from-file/start-import-books-from-file.handler.ts
@@ -4,6 +4,7 @@ import { EnvService } from 'src/libs/env/env.service';
 import { join } from 'path';
 import * as ExcelJs from 'exceljs';
 import { ImportBooksProgressStatusEvent } from '../import-books-progress-status/import-books-progress-status.event';
+import { createReadStream } from 'fs';
 
 @EventsHandler(StartImportBooksFromFileEvent)
 export class StartImportBooksFromFileHandler
@@ -15,38 +16,39 @@ export class StartImportBooksFromFileHandler
   ) {}
 
   async handle(event: StartImportBooksFromFileEvent) {
-    const workbook = new ExcelJs.Workbook();
-    await workbook.xlsx.readFile(
-      join(this.envService.get('TEMP_FOLDER'), event.filename),
+    const workbook = new ExcelJs.stream.xlsx.WorkbookReader(
+      createReadStream(
+        join(this.envService.get('TEMP_FOLDER'), event.filename),
+      ),
+      {},
     );
-
-    const worksheet = workbook.worksheets[0];
-    const totalRows = worksheet.actualRowCount - 1;
-    let isFirst = true;
     const expectedHeaders = ['name', 'pages', 'author'];
 
-    worksheet.eachRow((row, rowNumber) => {
-      if (!row || !row.values || !Array.isArray(row.values)) return;
+    for await (const worksheet of workbook) {
+      let isFirst = true;
+      let rowNumber = 0;
 
-      const values = row.values.slice(1) as string[];
+      for await (const row of worksheet) {
+        if (!row || !row.values || !Array.isArray(row.values)) continue;
 
-      if (isFirst) {
-        const allFieldsExists = values.every((value) =>
-          expectedHeaders.includes(value),
+        const values = row.values.slice(1) as string[];
+        rowNumber++;
+
+        if (isFirst) {
+          const allFieldsExists = values.every((value) =>
+            expectedHeaders.includes(value),
+          );
+          if (!allFieldsExists) throw new Error('Deu ruim aqui');
+          isFirst = false;
+          continue;
+        }
+
+        this.eventBus.publish(
+          new ImportBooksProgressStatusEvent(2_000_000, rowNumber - 1),
         );
-        if (!allFieldsExists) throw new Error('Deu ruim aqui');
-        isFirst = false;
-        return;
       }
 
-      this.eventBus.publish(
-        new ImportBooksProgressStatusEvent(totalRows, rowNumber - 1),
-      );
-
-      // const jsonData = expectedHeaders.reduce((acc, key, index) => {
-      //   acc[key] = values[index] || null;
-      //   return acc;
-      // }, {});
-    });
+      break;
+    }
   }
 }
